75c631092dd50c6251b777ba2756fca8e9e708c9
Pedro Medeiros <pedropwks@gmail.com>
2023-09-12 19:28:58 -0300

    Feat: Classe Archetype

---
+++ src/Archetypes/Archetype.ts
@@ -0,0 +1,27 @@
+export type EnergyType = 'mana' | 'stamina';
+
+abstract class Archetype {
+  private _name: string; 
+  private _special: number; 
+  private _cost: number;
+
+  constructor(name: string, special = 0, cost = 0) {
+    this._name = name;
+    this._special = special;
+    this._cost = cost;
+  }
+
+  get name(): string { return this._name; }
+
+  get special(): number { return this._special; }
+
+  get cost(): number { return this._cost; }
+
+  static createdArchetypeInstances(): number { 
+    throw new Error('Not implemented'); 
+  }
+
+  abstract get energyType(): EnergyType;
+}
+
+export default Archetype;
