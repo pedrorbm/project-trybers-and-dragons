60f9f00faf9b6305a8a04c37d3119d856c3b0b16
Pedro Medeiros <pedropwks@gmail.com>
2023-09-12 11:41:07 -0300

    Iniciando o projeto

--- src/Battle/Battle.ts
+++ src/Battle/Battle.ts
@@ -3,7 +3,7 @@
 // abstract class Battle {
 //   constructor(protected player: Fighter) { }
 
-//   // Should return 1 if player wins, -1 otherwise
+//   //Should return 1 if player wins, -1 otherwise
 //   fight(): number {
 //     return this.player.lifePoints === -1 ? -1 : 1;
 //   }
