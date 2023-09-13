aa869f171b1267f3e29c9f88fabad0b567b1d029
Pedro Medeiros <pedropwks@gmail.com>
2023-09-13 05:33:21 -0300

    Feat: Classe PVP

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,27 @@
+import Fighter from '../Fighter';
+import Battle from './Battle';
+
+class PVP extends Battle {
+  playerOne: Fighter;
+  playerTwo: Fighter;
+
+  constructor(playerOne: Fighter, playerTwo: Fighter) {
+    super(playerOne);
+    this.playerOne = playerOne;
+    this.playerTwo = playerTwo;
+  }
+
+  fight(): number {
+    if (this.playerOne.lifePoints <= 0) return -1;
+    if (this.playerTwo.lifePoints <= 0) return 1;
+
+    if (this.playerOne.lifePoints > -1 || this.playerTwo.lifePoints > -1) {
+      this.playerOne.attack(this.playerTwo);
+      this.playerTwo.attack(this.playerOne);
+    }
+    
+    return this.fight();
+  }
+}
+
+export default PVP;
